a4c3d937713221281008f9b41b88dd90b2d3ee0f
Rene Grob <rene_grob@epam.com>
2023-09-13 17:01:41 +0200

    Add test script for Docker container

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./gradlew clean quarkusBuild -Dquarkus.container-image.group=renegrob -Dquarkus.container-image.build=true -Dquarkus.container-image.labels.SHOULD_STOP=true
+
+BACK_PID=$!
+wait $BACK_PID
+
+docker network create test
+
+docker run -p8080:8080 --net=test -d renegrob/infinispan-embedded-demo:1.0.0-SNAPSHOT
+docker run -p8081:8080 --net=test -d renegrob/infinispan-embedded-demo:1.0.0-SNAPSHOT
+docker run -p8082:8080 --net=test -d renegrob/infinispan-embedded-demo:1.0.0-SNAPSHOT
+
+sleep 10s
+
+curl -s http://localhost:8080/cache/test1 | jq
+curl -s http://localhost:8080/cache/test2 | jq
+curl -s http://localhost:8080/cache/test3 | jq
+curl -s http://localhost:8080/cache/test4 | jq
+
+docker stop $(docker ps --filter=label='SHOULD_STOP=true' -q)
