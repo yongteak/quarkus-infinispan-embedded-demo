24d17efc19b61e596373e5e3a853a467bb7d3dbe
용택 권 <jelly@retgames.co.kr>
2024-03-19 15:29:52 +0900

    ..

--- test1.sh
+++ test1.sh
@@ -7,7 +7,7 @@ nc -v ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com 7800
 # native
 ./nstart.sh --node-prefix=1 --initial_hosts="192.168.12.113[7800],192.168.12.113[7801],192.168.12.113[7803],192.168.12.113[7804]"
 
-./nstart.sh --node-prefix=1 --initial_hosts="ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7800],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7801],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7803],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7804]"
+./nstart.sh --node-prefix=1 --bind-address="3.38.112.179" --initial_hosts="ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7800],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7801],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7803],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7804]"
 
 ./nstart.sh --node-prefix=2 --initial_hosts="ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7800],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7801],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7803],ec2-3-38-112-179.ap-northeast-2.compute.amazonaws.com[7804]"
 
